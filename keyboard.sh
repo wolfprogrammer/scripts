fcc0256d7fc2dd10fce0483cc79b40b9b000bf2c
Live session user <caiorss.rodrigues@gmail.com>
2013-07-05 03:25:28 -0300

    Keyboard.sh added

---
+++ keyboard.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# FILE: keyboard.sh
+# DESCRIPTION: Set keyboard to non english language
+#
+
+# loadkeys br-abnt2
+
+# Set the keyboard to BR-Portuguese in X server
+setxkbmap -model abnt2 -layout br -variant abnt2
